978ef12925c18f2b99276086b1e1dd98cd73508d
minnukota381 <minnukota381@gmail.com>
2024-06-17 13:06:47 +0530

    improved performance

--- app.js
+++ app.js
@@ -9,19 +9,15 @@ ffmpeg.setFfmpegPath(ffmpegPath);
 const app = express();
 const port = 3000;
 
-// Set EJS as the templating engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route to render the index page
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-// Route to handle video search and display quality options
 app.get('/search', async (req, res) => {
     const videoURL = req.query.url;
     if (!videoURL) {
@@ -40,7 +36,6 @@ app.get('/search', async (req, res) => {
     }
 });
 
-// Route to handle video streaming
 app.get('/stream', async (req, res) => {
     const videoURL = req.query.url;
     const quality = req.query.quality;
@@ -56,10 +51,8 @@ app.get('/stream', async (req, res) => {
             return res.status(404).send('No such format found');
         }
 
-        // Set content type to video/mp4 for streaming
         res.header('Content-Type', 'video/mp4');
 
-        // Use ytdl to directly pipe the video stream to the response
         ytdl(videoURL, { format: format })
             .on('error', (err) => {
                 console.error('Error:', err);
@@ -72,7 +65,6 @@ app.get('/stream', async (req, res) => {
     }
 });
 
-// Route to handle video download
 app.get('/download', async (req, res) => {
     const videoURL = req.query.url;
     const quality = req.query.quality;
@@ -91,7 +83,6 @@ app.get('/download', async (req, res) => {
 
         res.header('Content-Disposition', `attachment; filename="${title}.mp4"`);
 
-        // Use ytdl to directly pipe the video stream to the response
         ytdl(videoURL, { format: format })
             .on('error', (err) => {
                 console.error('Error:', err);
